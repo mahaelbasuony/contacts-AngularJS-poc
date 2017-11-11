d5c4907fe72a8ff1794a07f67b4ceefd7c841a70
Maha Elbasuony <mahaelbasuony@gmail.com>
2017-11-11 14:49:00 +0100

    Seperated angular app

---
+++ src/scripts/app.js
@@ -0,0 +1,10 @@
+angular.module('myApp', []).controller('searchCtrl', function ($scope) {
+  $scope.contacts = [
+    { name: "Maha", age: 27 }, { name: "Medhat", age: 30 }, { name: "Shahd", age: 35 }, { name: "Medhat", age: 44 },
+    { name: "Papa", age: 20 }, { name: "Adam", age: 10 }, { name: "Roka", age: 15 }, { name: "Naglaa", age: 54 },
+    { name: "Soha", age: 84 }, { name: "Rasha", age: 55 }, { name: "Heba", age: 40 }, { name: "Mahmoud", age: 30 },
+    { name: "Yousef", age: 70 }, { name: "Mai", age: 65 }, { name: "Maha", age: 64 }, { name: "Samira", age: 85 },
+    { name: "Seham", age: 90 }, { name: "Eman", age: 100 }, { name: "Amira", age: 95 }, { name: "Hamza", age: 12 }
+  ];
+
+});
